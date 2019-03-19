5dd9848f32b255c0f75db14243e26f7a98a24913
carlos <carlos.castaneda.gutierrez@gmail.com>
2019-03-18 21:31:34 -0500

    Implements teams selection

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/players' component={Players} />
-            <Route path='/teams' exact component={Teams} />
+            <Route path='/teams' component={Teams} />
             <Route render={() => <h1 className='text-center'>Four oh Four (404)</h1>} />
           </Switch>
         </div>
