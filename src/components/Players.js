e7598e50cf968f71f726a9b0a6dbcb35f8b66dac
carlos <carlos.castaneda.gutierrez@gmail.com>
2019-03-20 00:31:30 -0500

    Implements soft player transitions

--- src/components/Players.js
+++ src/components/Players.js
@@ -4,7 +4,10 @@ import Sidebar from './Sidebar'
 import { getPlayers } from '../api'
 import { parse } from 'query-string'
 import slug from 'slug'
-
+import {
+  TransitionGroup,
+  CSSTransition
+} from 'react-transition-group'
 
 export default class Players extends Component {
   state = {
@@ -49,58 +52,62 @@ export default class Players extends Component {
 
           const {
             name, position, teamId, number, avatar, apg, ppg, rpg, spg
-          } = players.find((player)=>slug(player.name) === match.params.playerId)
+          } = players.find((player) => slug(player.name) === match.params.playerId)
 
           return (
-            <div className='panel'>
-              <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`}/>
-              <h1 className='medium-header'>{name}</h1>
-              <h3 className='header'>#{number}</h3>
-              <div className='row'>
-                <ul className='info-list' style={{marginRight: 80}}>
-                  <li>
-                    Team
+            <TransitionGroup className='panel'>
+              <CSSTransition key={location.key} timeout={500} classNames='fade'>
+                <div className='panel'>
+                  <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
+                  <h1 className='medium-header'>{name}</h1>
+                  <h3 className='header'>#{number}</h3>
+                  <div className='row'>
+                    <ul className='info-list' style={{ marginRight: 80 }}>
+                      <li>
+                        Team
                     <div>
-                      <Link to={`/${teamId}`} style={{color:'#68809a'}}>
-                      {teamId[0].toUpperCase() + teamId.slice(1)}
-                      </Link>
-                    </div>
-                  </li>
-                  <li>
-                    Position
+                          <Link to={`/${teamId}`} style={{ color: '#68809a' }}>
+                            {teamId[0].toUpperCase() + teamId.slice(1)}
+                          </Link>
+                        </div>
+                      </li>
+                      <li>
+                        Position
                     <div>
-                      {position}
-                    </div>
-                  </li>
-                  <li>
-                    PPG
+                          {position}
+                        </div>
+                      </li>
+                      <li>
+                        PPG
                     <div>
-                      {ppg}
-                    </div>
-                  </li>
-                </ul>
-                <ul className='info-list'>
-                  <li>
-                    APG
+                          {ppg}
+                        </div>
+                      </li>
+                    </ul>
+                    <ul className='info-list'>
+                      <li>
+                        APG
                     <div>
-                      {apg}
-                    </div>
-                  </li>
-                  <li>
-                    SPG
+                          {apg}
+                        </div>
+                      </li>
+                      <li>
+                        SPG
                     <div>
-                      {spg}
-                    </div>
-                  </li>
-                  <li>
-                    RPG
+                          {spg}
+                        </div>
+                      </li>
+                      <li>
+                        RPG
                     <div>
-                      {rpg}
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
+                          {rpg}
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
           )
         }} />
       </div>
